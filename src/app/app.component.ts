64c8e7c9a0e45a25fc85eb35ca51f7071ac29f36
Julia Nowicka <julia.nowicka@lemondev.pl>
2025-08-20 08:53:21 +0200

    feat(app): add root app component and index.html

---
+++ src/app/app.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './core/components/header/header.component';
+import { FooterComponent } from './core/components/footer/footer.component';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, HeaderComponent, FooterComponent],
+  styleUrls: ['./app.component.scss'],
+  template: `
+    <div class="app-container">
+      <app-header></app-header>
+      <main class="app-content">
+        <router-outlet></router-outlet>
+      </main>
+      <app-footer></app-footer>
+    </div>
+  `,
+})
+export class AppComponent {
+  title = 'TaskFlow';
+}
