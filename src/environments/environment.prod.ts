cb1403a680195152de232cfcac681a8cf6fccaa5
Julia Nowicka <julia.nowicka@lemondev.pl>
2025-08-20 08:55:31 +0200

    chore(env): add environment configuration

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: true,
+  apiUrl: '', // Local-first for now
+  appName: 'TaskFlow',
+  version: '1.0.0',
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: false,
+  apiUrl: '', // Local-first for now
+  appName: 'TaskFlow',
+  version: '1.0.0',
+};
